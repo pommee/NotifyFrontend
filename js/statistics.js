7ca6bd535ab2508ef94bee3af64183e056b6e683
pommee <hugoh@hotmail.se>
2022-05-21 16:08:38 +0200

    Bar chart instead of lines

--- js/statistics.js
+++ js/statistics.js
@@ -1,40 +1,3 @@
-var xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-
-new Chart(document.querySelector("#statisticsChart"), {
-    type: "line",
-
-    data: {
-        labels: xValues,
-        datasets: [{
-            label: "Total Users",
-            data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-            borderColor: "red",
-            fill: false
-        }, {
-            label: "Page Visits",
-            data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-            borderColor: "green",
-            fill: false
-        }, {
-            label: "Total Notes",
-            data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-            borderColor: "blue",
-            fill: false
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                labels: {
-                    color: 'white'
-                },
-                display: true
-            },
-        }
-    },
-});
-
 function fetchStatistics() {
     fetch("http://localhost:8080/api/statistics", {
         method: "GET",
@@ -51,6 +14,44 @@ function setData(data) {
     document.querySelector("#totalUsers").innerHTML = data.totalUsers;
     document.querySelector("#pageVisitsNumber").innerHTML = data.pageVisits.visitorCount;
     document.querySelector("#totalNotesNumber").innerHTML = data.totalNotes;
+    setChart(data.totalUsers, data.pageVisits.visitorCount, data.totalNotes)
 }
 
 fetchStatistics()
+
+function setChart(totalUsers, visitorCount, totalNotes) {
+    new Chart(document.querySelector("#statisticsChart"), {
+        type: 'bar',
+        data: {
+            labels: ['Total Users', 'Page Visits', 'Total Notes'],
+            datasets: [{
+                label: 'Total Users',
+                data: [totalUsers, visitorCount, totalNotes],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
